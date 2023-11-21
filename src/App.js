7d6618303e1fa09027ac9eb5c158c11f7ec2ba37
Vijay Maurya <vijay@techpath.biz>
2023-11-21 11:55:32 +0530

    restaurant

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Restaurant from './component/basics/Restaurant'
+
+const App = () => {
+  return (
+    <div>
+      
+      <Restaurant/>
+      
+    </div>
+  )
+}
+
+export default App;
---
+++ src/component/basics/menuApi.js
@@ -0,0 +1,142 @@
+
+
+const Menu = [
+{
+    id: 1,
+    image: "images/maggi.jpg",
+    name: "maggi",
+    category: "breakfast",
+    price: "12rs",
+    description: " Here we see that an element with greater stack order is always above an element with a lower stack order",
+},
+{
+    id: 2,
+    image: "images/allupakoida.jpg",
+    name: "Allupakoida",
+    category: "breakfast",
+    price: "12rs",
+    description: " Here we see that an element with greater stack order is always above an element with a lower stack order",
+},
+
+{
+    id: 3,
+    image: "images/chola.jpg",
+    name: "Chola",
+    category: "lunch",
+    price: "12rs",
+    description: " Here we see that an element with greater stack order is always above an element with a lower stack order",
+},
+
+{
+    id: 4,
+    image: "images/corn.jpg",
+    name: "Corn",
+    category: "evening",
+    price: "12rs",
+    description: " Here we see that an element with greater stack order is always above an element with a lower stack order",
+},
+
+{
+    id: 5,
+    image: "images/momo.jpg",
+    name: "Momo",
+    category: "evening",
+    price: "12rs",
+    description: " Here we see that an element with greater stack order is always above an element with a lower stack order",
+},
+
+{
+    id: 6,
+    image: "images/nonvegthali.jpg",
+    name: "Non-Vegthali",
+    category: "dinner",
+    price: "12rs",
+    description: " Here we see that an element with greater stack order is always above an element with a lower stack order",
+},
+
+{
+    id: 7,
+    image: "images/paubhaji.jpg",
+    name: "Paubhaji",
+    category: "breakfast",
+    price: "12rs",
+    description: " Here we see that an element with greater stack order is always above an element with a lower stack order",
+},
+
+{
+    id: 8,
+    image: "images/pizza.jpg",
+    name: "Pizza",
+    category: "evening",
+    price: "12rs",
+    description: " Here we see that an element with greater stack order is always above an element with a lower stack order",
+},
+
+{
+    id: 9,
+    image: "images/puri.jpg",
+    name: "Puri",
+    category: "dinner",
+    price: "12rs",
+    description: " Here we see that an element with greater stack order is always above an element with a lower stack order",
+},
+
+{
+    id: 10,
+    image: "images/rajmarice.jpg",
+    name: "Rajmarice",
+    category: "dinner",
+    price: "12rs",
+    description: " Here we see that an element with greater stack order is always above an element with a lower stack order",
+},
+
+{
+    id: 11,
+    image: "images/rotiwithmeat.jpg",
+    name: "Rotimeat",
+    category: "lunch",
+    price: "12rs",
+    description: " Here we see that an element with greater stack order is always above an element with a lower stack order",
+},
+
+{
+    id: 12,
+    image: "images/samosa.jpg",
+    name: "Samosa",
+    category: "evening",
+    price: "12rs",
+    description: " Here we see that an element with greater stack order is always above an element with a lower stack order",
+},
+
+{
+    id: 13,
+    image: "images/sweet.jpg",
+    name: "Desert",
+    category: "desert",
+    price: "12rs",
+    description: " Here we see that an element with greater stack order is always above an element with a lower stack order",
+},
+
+
+
+{
+    id: 12,
+    image: "images/vegthali.jpg",
+    name: "Vegthali",
+    category: "dinner",
+    price: "12rs",
+    description: " Here we see that an element with greater stack order is always above an element with a lower stack order",
+},
+
+{
+    id: 13,
+    image: "images/maggi.jpg",
+    name: "maggi",
+    category: "vijay",
+    price: "12rs",
+    description: " Here we see that an element with greater stack order is always above an element with a lower stack order",
+}
+
+];
+
+export default Menu;
